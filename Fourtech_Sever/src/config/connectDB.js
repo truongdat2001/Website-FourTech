19c9e2ce282a39163c59c22e107323417f5c150a
truongdat2001 <dattruong792001@gmail.com>
2025-05-19 23:52:45 +0700

    update

--- Fourtech_Sever/src/config/connectDB.js
+++ Fourtech_Sever/src/config/connectDB.js
@@ -2,12 +2,14 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config
 
 const database_host = process.env.DB_HOST;
+const database_port = process.env.DB_PORT;
 const database_name = process.env.DB_NAME;
 const database_user = process.env.DB_USER;
 const database_password = process.env.DB_PASSWORD;
 
 const sequelize = new Sequelize(database_name, database_user, database_password, {
     host: database_host,
+    port: database_port,
     dialect: 'mysql',
     logging: false
 });
